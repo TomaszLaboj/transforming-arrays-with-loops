d24dc94e9a41bb10d64ef4b7e20af02dc215276f
TomaszLaboj <tomasz.laboj@gmail.com>
2023-06-17 21:32:09 +0100

    pseudo code added

---
+++ 1c-count-strings.js
@@ -0,0 +1,11 @@
+/*
+Write a function that takes an array of strings, 
+and returns an array with the number of characters in each string.
+*/
+
+/*
+function countCharacter(array)
+init new array
+loop through each element and return lenght of the string
+return new array
+*/
