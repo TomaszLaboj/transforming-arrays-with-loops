472a13fdeb0907d4c556eca1a7bc9856b4669383
TomaszLaboj <tomasz.laboj@gmail.com>
2023-06-17 21:19:05 +0100

    pseudocode added

---
+++ 1b-Invert numbers.js
@@ -0,0 +1,13 @@
+/*Challenge: Write a function that takes an array of numbers,
+ and returns an array with the same numbers, but signs flipped.
+*/
+
+/*
+function invertNumbers(array){
+    intit new array
+    loop through every element and multiply by -1
+    push result to new array
+    return new array
+    
+}
+*/
