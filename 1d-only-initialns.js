7a1ddc6251559c7a47656a7f6ca7e268dcfee2cd
TomaszLaboj <tomasz.laboj@gmail.com>
2023-06-17 21:36:40 +0100

    pseudocode added

---
+++ 1d-only-initialns.js
@@ -0,0 +1,12 @@
+/*
+Write a function that takes an array of names (always <firstname> <surname> format) 
+and returns an array of initials for each person.
+*/
+
+/*
+function checkInititals(array)
+init new array
+loop through all the elements and split them using string method
+return new array
+
+*/
