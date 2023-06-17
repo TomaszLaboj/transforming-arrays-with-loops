57c86b3bd604d2cdeb97eaaa78d8b4adc5f97579
TomaszLaboj <tomasz.laboj@gmail.com>
2023-06-17 21:42:15 +0100

    js implementation

--- 1d-only-initialns.js
+++ 1d-only-initialns.js
@@ -6,9 +6,19 @@ and returns an array of initials for each person.
 /*
 function checkInititals(array)
 init new array
-loop through all the elements and split them using string method
+loop through all the elements and split them using string method and interpolation
 return new array
 
 */
+function checkInitials(array){
+    const newArray = [];
+    for (element of array){
+        let FirstSecond = element.split(' ');
+        newArray.push(`${FirstSecond[0]}.${FirstSecond[1]}.`);
+        
+    }
+
+    return newArray;
+}
 
 console.log(checkInitials(["Ash Ketchum", "Lucy Heartfilia", "Yugi Moto"]), 'should be ', ["A.K.", "L.H.", "Y.M."])
