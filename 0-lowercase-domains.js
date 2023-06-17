e6b5ecaedd7e3ad69e47f2d05399f28c323cc49c
TomaszLaboj <tomasz.laboj@gmail.com>
2023-06-17 20:17:59 +0100

    pseudocode with function signature added

---
+++ 0-lowercase-domains.js
@@ -0,0 +1,12 @@
+/*
+function transformToLowerCase(array)
+
+loop takes every element of the input array and applies a string method 'to lower case'
+
+push the transformed element to an empty array
+
+return transformed array
+
+
+*/
+
