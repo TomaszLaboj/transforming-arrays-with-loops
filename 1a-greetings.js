dfb91e8f218aa107685f9c517015ab747528e160
TomaszLaboj <tomasz.laboj@gmail.com>
2023-06-17 21:02:35 +0100

    pseudocode added

---
+++ 1a-greetings.js
@@ -0,0 +1,11 @@
+/*
+Challenge: Write a function that takes an array of names, 
+and returns an array with a greeting for each of those names: "Hello, <name>!".
+*/
+/*
+function addGreeting(array){
+    a loop takes every element and adds a string before every element using the string interpolation
+    push new string to a new array
+    return new array
+}
+*/
