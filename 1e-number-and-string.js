4fa96790829b51b2d7f2ba1277adf42a064ae726
TomaszLaboj <tomasz.laboj@gmail.com>
2023-06-17 21:46:10 +0100

    pseudocode added

---
+++ 1e-number-and-string.js
@@ -0,0 +1,13 @@
+/*
+Write a function that takes an array of numbers 
+and returns an array of plain objects, with properties:
+*/
+
+/*
+function transformToString(array)
+init new array 
+loop through every element and push number to the object and convert number to a string
+
+return new array
+*/
+
