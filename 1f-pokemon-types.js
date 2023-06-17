c7a8f2055c05b5ecb20cfa8390a5d22ab1279d22
TomaszLaboj <tomasz.laboj@gmail.com>
2023-06-17 22:39:38 +0100

    pseudocode added

---
+++ 1f-pokemon-types.js
@@ -0,0 +1,15 @@
+/*
+Write a function that takes an array of objects representing Pokémon,
+ with string properties name and type, 
+and returns a descriptive sentence about them and their type: 
+"<name> is a <type in lowercase> type Pokémon.".
+*/
+
+/*
+function describePokemon(array){
+    init newArray
+    loop through each element in the array and access the name and type properties, 
+    push a interpolated string to the new array changing the type to lower case
+    return new array
+}
+*/
